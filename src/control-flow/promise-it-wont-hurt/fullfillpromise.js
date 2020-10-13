7688d8614cb82212211e632e0cc63e5b28bb7681
Yeray Medina López <ymedlop@gmail.com>
2020-10-13 21:23:46 +0200

    Add fullfill promise scenario

---
+++ src/control-flow/promise-it-wont-hurt/fullfillpromise.js
@@ -0,0 +1,11 @@
+'use strict';
+
+require('es6-promise');
+
+var promise = new Promise(function (fulfill, reject) {
+    // Your solution here
+    setTimeout(() => fulfill('FULFILLED!'), 300);
+});
+
+// Your solution here
+promise.then(console.log);
